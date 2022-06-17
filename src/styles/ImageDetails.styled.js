b4ccdf170e74b355c46d3cb5e5eaaa5635c95a2d
Andrius Murauskas <andrius.murauskas.am@gmail.com>
2022-06-17 08:18:11 +0300

    removed image height 100%

--- src/styles/ImageDetails.styled.js
+++ src/styles/ImageDetails.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledImageDetails = styled.div`
    position: relative;
-   height: 85gitvh;
+   height: 85vh;
    margin: 1rem 0;
    div {
     background-color: #000;
@@ -11,15 +11,12 @@ export const StyledImageDetails = styled.div`
     padding: 0.25rem 1rem 0 1rem;
    }
    img {
-    height: 100%;
     max-width: 20rem;
     margin: 0 0.5rem;
     border-radius: 0.2rem;
    }
-   .albumTitle:hover::after {
-      position: absolute;
-      top: 20px;
-      content: 'open';
+   .albumTitle:hover {
+      border: 1px solid #fff;
    }
   
 `
